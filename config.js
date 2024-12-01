967ebdbd517eb7f3c2fb459dde962ecf1871dce3
Koyeb-LK <dilishadilisha29@gmail.com>
2024-12-01 12:32:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=LrwCCCJS#D9PQlwXvh9PXe-iI49xWme3n0wddC8E3sXP4WxWPB00",
 MONGODB: process.env.MONGODB || "mongodb://mongo:RMzLlxhpMeeNOOWmuAuERAPDxUcSdOZo@autorack.proxy.rlwy.net:41909",
-GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? '': process.env.GITHUB_USERNAME,
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '': process.env.GITHUB_AUTH_TOKEN
+GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'Koyeb-LK': process.env.GITHUB_USERNAME,
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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': process.env.GITHUB_AUTH_TOKEN
 };
