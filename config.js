1b9aa5b1c327d4592646d4772890c04093f47980
Koyeb-LK <dilishadilisha29@gmail.com>
2024-12-01 09:51:53 +0530

    💥  D  O  N  T  💥

---
+++ config.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || " Put Your Session Id",
+MONGODB: process.env.MONGODB || "mongodb+srv://athulakumara604:qBwvqo6IM64eT1SL@cluster0.wr7rx.mongodb.net/",
+GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'your github name': process.env.GITHUB_USERNAME,
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? ' your github auth tovken ': process.env.GITHUB_AUTH_TOKEN
+};
